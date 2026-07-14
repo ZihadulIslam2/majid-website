b92b7652d91fb9b0db96bf37621a7622bf32fd9c
zihadulislam2 <zihadul708@gmail.com>
2026-07-14 22:06:50 +0600

    fix: deploy error

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: "majid-website",
+      name: "imoscan-frontend",
       cwd: __dirname,
       script: "node_modules/next/dist/bin/next",
       args: "start -p 3000",
